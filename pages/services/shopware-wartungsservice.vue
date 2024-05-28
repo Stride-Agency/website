1cb73edd7987231ad993f8d10f71bedccf690cf8
phillipmohr <phillip.mohr97@gmail.com>
2024-05-28 20:19:38 +0200

    remove text

--- pages/services/shopware-wartungsservice.vue
+++ pages/services/shopware-wartungsservice.vue
@@ -5,7 +5,7 @@
           <div class="mx-auto max-w-2xl text-center relative">
 
             <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Rundum-Wartung für deinen Shop</h1>
-            <p class="mt-6 text-lg leading-8 text-gray-300 mb-12">Erhalte regelmäßige Updates und professionelle Wartung für deinen Shopware-Shop. So bleibt dein Online-Geschäft sicher, schnell und stets auf dem neuesten Stand. Verlass dich auf unsere Expertise für einen reibungslosen Betrieb.</p>
+            <p class="mt-6 text-lg leading-8 text-gray-300 mb-12">Erhalte regelmäßige Updates und professionelle Wartung für deinen Shopware-Shop. So bleibt dein Online-Geschäft sicher, schnell und stets auf dem neuesten Stand.</p>
 
             <stripe-pricing-table pricing-table-id="prctbl_1PKxJwJBWr5bnLdv6sLQUCCF"
             publishable-key="pk_live_51PBsnCJBWr5bnLdveCAAp42TWQ73q4SgU0bzelyT1j0T8nQJ1NspsJMTM40yhIUw6KOIKK7d7F3fles3tHmw3KXy00I0xLFpJS">
