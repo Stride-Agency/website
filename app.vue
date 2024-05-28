c0d90e718db11e830900f53fa867295d7e838fea
Jonas <jonas@stride.agency>
2024-05-28 15:13:08 +0200

    Updated Data policy and fixed homepage

--- app.vue
+++ app.vue
@@ -7,11 +7,33 @@
 </template>
 
 <script setup>
-
-
 useHead({
   bodyAttrs: {
-    class: 'bg-slate-900'
+    class: 'bg-slate-900 text-gray-300'
   },
 });
 </script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer base {
+  h1 {
+    @apply text-2xl mb-2 font-bold;
+  }
+  h2 {
+    @apply text-xl mb-2 font-bold;
+  }
+  h3 {
+    @apply text-lg mb-2 font-bold;
+  }
+  p {
+    @apply mb-6;
+  }
+  ul {
+    @apply list-disc pl-4 mb-6;
+  }
+}
+</style>
