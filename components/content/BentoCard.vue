2053e0e9da4b5517147e99ce3f34b595601d1793
phillip <phillip@newwwagency.com>
2024-12-03 13:38:46 +0100

    bento card

---
+++ components/content/BentoCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="cardClasses">
+        <div class="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
+            <div>
+                <ContentSlot :use="$slots.image" imgClasses="" />
+            </div>
+            <div class="p-10">
+                <p class="mt-2 text-lg font-medium tracking-tight text-white">{{ title }}</p>
+                <p class="mt-2 max-w-lg text-sm/6 text-gray-400">
+                    {{ description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    },
+    cardClasses: {
+        type: String,
+        default: 'flex p-px lg:col-span-4'
+    },
+    imageClasses: {
+        type: String,
+        default: 'h-80 object-cover object-left'
+    }
+})
+</script>
--- components/content/BentoGrid.vue
+++ components/content/BentoGrid.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <div class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
         <ContentSlot :use="$slots.default" />
     </div>
   </template>
 
 <script setup lang="ts">
-    console.log('$slots.default', $slots.default)
+
 </script>
