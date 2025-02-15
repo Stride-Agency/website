1540f052a612dcd15beeec29a77092a4f1772076
phillip <phillip@newwwagency.com>
2025-02-15 18:50:22 +0100

    bento grid text content

--- components/content/BentoGrid.vue
+++ components/content/BentoGrid.vue
@@ -1,5 +1,13 @@
 <template>
     <Container size="lg">
+        <Container v-if="title || description" size="xs" class="text-center space-y-10">
+            <Heading v-if="title" tag="h2">
+                {{ title }}
+            </Heading>
+            <p v-if="description" class="text-balance text-lg tracking-tight">
+                {{ description }}
+            </p>
+        </Container>
         <div class="mt-10 grid gap-4 sm:mt-16" :class="gridClasses">
             <ContentSlot :use="$slots.default" />
         </div>
@@ -8,6 +16,12 @@
 
 <script setup lang="ts">
 defineProps({
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
     gridClasses: {
         type: String,
         required: true
