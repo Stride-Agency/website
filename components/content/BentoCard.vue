2280b5df9aeac9cb744b451dd80d753359e2c027
phillip <phillip@newwwagency.com>
2025-02-15 18:31:16 +0100

    card wrapper classes

--- components/content/BentoCard.vue
+++ components/content/BentoCard.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex p-px" :class="cardClasses">
-        <div class="w-full overflow-hidden rounded-lg bg-neutral-900 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
+        <div class="w-full overflow-hidden rounded-3xl bg-neutral-900 ring-1 ring-white/15" :class="cardWrapperClasses">
             <div class="[&>img]:object-cover [&>img]:object-left [&>img]:h-full [&>img]:w-full">
                 <ContentSlot :use="$slots.image" unwrap="p" />
             </div>
-            <div class="px-10 py-5">
+            <div class="px-10 py-5 mt-auto">
                 <p class="mt-2 font-medium tracking-tight text-neutral-100 text-[2rem]">{{ title }}</p>
                 <p class="mt-2 max-w-lg text-lg text-neutral-200">
                     {{ description }}
@@ -30,6 +30,10 @@ defineProps({
     cardClasses: {
         type: String,
         required: true
+    },
+    cardWrapperClasses: {
+        type: String,
+        default: ''
     },
     imageClasses: {
         type: String,
