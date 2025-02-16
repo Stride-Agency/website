391883a570efece628cc1015ec6cd0dbc90b8291
phillip <phillip@newwwagency.com>
2025-02-16 16:03:36 +0100

    introduce process item

---
+++ components/Process.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="relative">
+
+        <Container size="lg" class="py-28 relative">
+            <HeadingAndDescription v-if="title" :title="title" :description="description" />
+            <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-auto-fit lg:grid-cols-3 gap-6">
+                <ContentSlot :use="$slots.default" />
+            </div>
+        </Container>
+
+    </section>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+})
+</script>
---
+++ components/_ProcessItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <Card :heading="title" heading-size="m" heading-tag="div" :class="cardClasses">
+        <slot></slot>
+        {{ number }}
+    </Card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+    },
+    cardClasses: {
+        type: String,
+    }
+})
+</script>
